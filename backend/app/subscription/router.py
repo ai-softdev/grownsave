dba532c7f4594ca43e29b0aeee327d0e479fd6a6
User <arturikjan2004@gmail.com>
2024-09-21 23:54:19 +0500

    fix

--- backend/app/subscription/router.py
+++ backend/app/subscription/router.py
@@ -1,3 +1,4 @@
+import uuid
 from datetime import datetime, timedelta
 
 from fastapi import APIRouter, Depends
@@ -20,8 +21,6 @@ router = APIRouter(prefix="/subscription", tags=["Подписка"])
 
 @router.post('/purchase')
 async def implement_creating_subscription(data: PaymentData, user: User = Depends(get_current_user)) -> dict[str, str]:
-
-
     plan = await Plan.find_by_id_or_fail(model_id=data.plan_id)
 
     if await has_subscription(user, raise_exception=False):
@@ -29,25 +28,23 @@ async def implement_creating_subscription(data: PaymentData, user: User = Depend
 
     '''
     Imitation of creating payment bill
-    
+
     token_response = await request.post('<url for creating token>', data={'card_num': plan.card_num,
                                                                  'exp_month': data.exp_month,
                                                                  'exp_year': data.exp_year,
                                                                  'cvv': data.cvv})
     token = token_response['token']
-    
+
     create_transaction_response = await request.post('<url for creating transaction>', data={'token': token,
                                                                                     'amount': plan.price})
     transaction_id = create_transaction_response['transaction_id']
-    
-    order = await Order.create(transaction_id)
-    
-    purchase = await Purchase.create(order_id=order.id, user_id=user.id, plan_id=plan.id)
+
+
+    order = await Order.create(transaction_id=transaction_id, amount=plan.price, plan_id=plan.id, user_id=user.id)
+
     '''
+    await Order.create(transaction_id=str(uuid.uuid4()), amount=plan.price, plan_id=plan.id, user_id=user.id)
 
     return {
         'message': 'Order is created. Waiting to purchase'
     }
-
-
-
